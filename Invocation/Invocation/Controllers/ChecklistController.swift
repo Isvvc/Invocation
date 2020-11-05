0ae843ef853d8a8d43d5735d1ff3d97cdc03adc7
Isvvc <lyons@tuta.io>
2020-11-04 17:28:05 -0700

    Add date format settings

--- Invocation/Invocation/Controllers/ChecklistController.swift
+++ Invocation/Invocation/Controllers/ChecklistController.swift
@@ -9,12 +9,34 @@ import Foundation
 
 class ChecklistController: ObservableObject {
     
-    lazy var dateFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        formatter.timeStyle = .short
-        return formatter
-    }()
+    var dateFormatter: DateFormatter
+    
+    init() {
+        var dateStyle = UserDefaults.standard.integer(forKey: Defaults.dateStyle.rawValue)
+        var timeStyle = UserDefaults.standard.integer(forKey: Defaults.timeStyle.rawValue)
+        
+        // Style 0 is none (and the default value when installing the app).
+        // We want there to be a date and time, so set them to short by default.
+        if dateStyle == 0 {
+            dateStyle = 1
+            UserDefaults.standard.set(1, forKey: Defaults.dateStyle.rawValue)
+        }
+        if timeStyle == 0 {
+            timeStyle = 1
+            UserDefaults.standard.set(1, forKey: Defaults.timeStyle.rawValue)
+        }
+        
+        dateFormatter = DateFormatter()
+        setDateFormat(dateStyleInt: dateStyle, timeStyleInt: timeStyle)
+    }
+    
+    func setDateFormat(dateStyleInt: Int, timeStyleInt: Int) {
+        let dateStyle = DateFormatter.Style(rawValue: UInt(dateStyleInt)) ?? .short
+        let timeStyle = DateFormatter.Style(rawValue: UInt(timeStyleInt)) ?? .short
+        
+        dateFormatter.dateStyle = dateStyle
+        dateFormatter.timeStyle = timeStyle
+    }
     
     /// Sets a URL's protocol to HTTPS.
     ///
--- Invocation/Invocation/Views/SettingsView.swift
+++ Invocation/Invocation/Views/SettingsView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct SettingsView: View {
     
-    @AppStorage(Defaults.projectNameFill.rawValue) var projectNameFill: Bool = false
+    //MARK: Properties
+    
+    @AppStorage(Defaults.projectNameFill.rawValue) private var projectNameFill: Bool = false
+    @AppStorage(Defaults.dateStyle.rawValue) private var dateStyle: Int = 1
+    @AppStorage(Defaults.timeStyle.rawValue) private var timeStyle: Int = 1
+    
+    @EnvironmentObject private var checklistController: ChecklistController
+    
+    //MARK: Body
     
     var body: some View {
         Form {
@@ -23,15 +31,126 @@ struct SettingsView: View {
                     }
                 }
             }
+            
+            Section(header: Text("Date Format")) {
+                Picker("Date style", selection: $dateStyle) {
+                    VStack(alignment: .leading) {
+                        Text("Short")
+                        Text(dateFormatterShort.string(from: Date()))
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                    .tag(1)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Medium")
+                        Text(dateFormatterMedium.string(from: Date()))
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                    .tag(2)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Long")
+                        Text(dateFormatterLong.string(from: Date()))
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                    .tag(3)
+                }
+                .onChange(of: dateStyle){ dateStyle in
+                    checklistController.setDateFormat(dateStyleInt: dateStyle, timeStyleInt: timeStyle)
+                }
+                
+                Picker("Time style", selection: $timeStyle) {
+                    VStack(alignment: .leading) {
+                        Text("Short")
+                        Text(timeFormatterShort.string(from: Date()))
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                    .tag(1)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Medium")
+                        Text(timeFormatterMedium.string(from: Date()))
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                    .tag(2)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Long")
+                        Text(timeFormatterLong.string(from: Date()))
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                    .tag(3)
+                }
+                .onChange(of: timeStyle){ timeStyle in
+                    checklistController.setDateFormat(dateStyleInt: dateStyle, timeStyleInt: timeStyle)
+                }
+            }
         }
         .navigationTitle("Settings")
     }
+    
+    //MARK: Date Formatters
+    
+    // I feel like I shoudln't have to do this but
+    // idk how else to get 6 different date formats.
+    
+    private var dateFormatterShort: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter
+    }
+    
+    private var dateFormatterMedium: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }
+    
+    private var dateFormatterLong: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }
+    
+    private var timeFormatterShort: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        return formatter
+    }
+    
+    private var timeFormatterMedium: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .medium
+        return formatter
+    }
+    
+    private var timeFormatterLong: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .long
+        return formatter
+    }
+    
 }
+
+//MARK: Previews
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             SettingsView()
+                .previewDevice("iPhone 6S")
         }
     }
 }
